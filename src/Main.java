e6b5816142500433973b4c0fd54974a19b6bbdc7
javilier28 <jrosagomez40@gmail.com>
2025-02-20 19:25:20 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+public class main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int opcion = 0;
 
+        do {
+            System.out.println("1-Suma ");
+            System.out.println("2-Resta ");
+            System.out.println("3-Multiplicar por escalar ");
+            System.out.println("4-Reparación ");
+            System.out.println("5-Multiplicar dos matrices ");
+            System.out.println("6-Calcular transpuesta ");
+            System.out.println("7-Calcular determinante ");
+            System.out.println("8-Calcular la traza ");
+            System.out.println("9-Ver tipo de matriz ");
+            System.out.println("10-Calcular valores y vectores propios ");
+            System.out.println("11-Diagonalizar");
+            System.out.println("0-Finalizar");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+            }
+        } while (opcion != 0);
+        System.out.println("sistema finalizado con exito");
     }
 }
